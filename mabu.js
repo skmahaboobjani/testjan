6bca777b70f9cbfaa3df8b34785ef8011ffab403
Shaik Mahaboob jani <92629496+skmahaboobjani@users.noreply.github.com>
2022-01-23 18:17:25 +0530

    Add files via upload

---
+++ mabu.js
@@ -0,0 +1,65 @@
+var univList = []
+
+async function pushUniversitiesList()
+{
+    let list = document.getElementById("universities-list");
+    
+    await getUniversities().then(data => {
+        for (const key in data) {
+            univList.push([data[key].name, data[key].web_pages[0]])
+        }
+    })
+
+    univList.forEach((item) => {
+        let li = document.createElement("li");
+        li.innerHTML = item[0] + "<br>" + "Website: <a target='_blank' href='" + item[1] + "'>" + item[0] + "</a>"
+        li.classList.add("list-group-item");
+        list.appendChild(li);
+    });
+
+}
+
+function getUniversities(a)
+{
+    let data = fetch(`http://universities.hipolabs.com/search?country=india`)
+    .then(response => {
+        return response.json();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+    return data;
+}
+
+function searchUniversities()
+{   
+    let searchList = []
+    input = document.getElementsByTagName("input")[0].value;
+
+    searchList = univList.filter((item) => {
+       return item[0].toLowerCase().includes(input.toLowerCase())
+    })  
+
+    // console.log(searchList)
+    return updateUniversitiesList(searchList);
+}
+
+function updateUniversitiesList(searchList)
+{
+    let list = document.getElementById("universities-list");
+
+    let child = list.lastElementChild;
+
+    while (child) {
+        list.removeChild(child);
+        child = list.lastElementChild;
+    }
+
+    searchList.forEach(item => {
+        let li = document.createElement("li");
+        li.innerHTML = item[0] + "<br>" + "Website: <a target='_blank' href='" + item[1] + "'>" + item[0] + "</a>"
+        li.classList.add("list-group-item");
+        list.appendChild(li);
+    });
+}
